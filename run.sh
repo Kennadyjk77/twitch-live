bc09b76ff65e07981f24d80c2b4b68356496c4e3
Kennadyjk77 <153106139+Kennadyjk77@users.noreply.github.com>
2026-09-12 07:33:28 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-# Port Binding for Render Web Service
 python3 -m http.server 8080 &
 python3 /app/bot.py &
 
-# Unga M3U8 Live Stream Link
 STREAM_URL="https://newcdn.tamils.click/live1channel/tracks-v1a1/mono.ts.m3u8"
-
-echo "=== STARTING AUTOMATIC M3U8 RESTREAM TO TWITCH ==="
 
 while true
 do
@@ -17,6 +12,5 @@ do
     -pix_fmt yuv420p -g 60 -c:a aac -b:a 128k -ar 44100 \
     -f flv "rtmp://live.twitch.tv/app/$TWITCH_KEY"
   
-  echo "Stream disconnect aanaal 5 seconds-il automatic-a reconnect aagum..."
   sleep 5
 done
